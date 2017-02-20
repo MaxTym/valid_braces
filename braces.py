3f45e6517d3e78aa098882e88deab65b5db08702
Maksym Tymtsiv <mtymtsiv@gmail.com>
2017-02-20 14:18:57 -0500

    first commit

---
+++ braces.py
@@ -0,0 +1,17 @@
+def braces(s):
+    BRACES = { '(': ')', '[': ']', '{': '}' }
+    stack = []
+    for b in s:
+        for i in b:
+            c = BRACES.get(b)
+            if c:
+                stack.append(c)
+            elif not stack or stack.pop() != b:
+                return False
+    return not stack
+
+braces_input = input("Enter braces: \nValid braces: {}()[]\n")
+if braces(braces_input):
+    print("YES")
+else:
+    print("NO")
